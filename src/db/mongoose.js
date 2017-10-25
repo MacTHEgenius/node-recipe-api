b3ea8a13e9aeda636aa922ca833d3d1add3804c2
MacTHEgenius <levesque.benoit@me.com>
2017-10-24 23:21:13 -0400

    Update Travis with MongoDB

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/respice_api_db', { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/recipe_api_db', { useMongoClient: true });
 
 module.exports = {
     mongoose
