1659e617ad8d2f8424547df5f9528bdd663605be
MacTHEgenius <levesque.benoit@me.com>
2017-10-31 23:28:33 -0400

    Create Step model

---
+++ src/models/Step.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Recipe = require('./Recipe');
+const Schema = mongoose.Schema;
+
+const stepSchema = new Schema({
+    description: { type: String, required: true },
+    position: { type: String, required: true },
+    ingredients: { type: Schema.Types.ObjectId, ref: 'Ingredient' },
+    recipe: { type: Schema.Types.ObjectId, ref: 'Recipe' }
+});
+
+stepSchema.index({ position: 1, _id: 1 }, { unique: true });
+
+const Step = mongoose.model('Step', stepSchema);
+
+module.exports = Step;
